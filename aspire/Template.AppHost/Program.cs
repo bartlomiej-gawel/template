a11bb0071e06c4c45918b22359d3da3c9bb24bff
Bartłomiej Gaweł <bartlomiej.gawel@it-solve.pl>
2025-07-07 19:55:18 +0200

    add postgres

--- aspire/Template.AppHost/Program.cs
+++ aspire/Template.AppHost/Program.cs
@@ -2,6 +2,12 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+var postgresUsername = builder.AddParameter("postgres-username", true);
+var postgresPassword = builder.AddParameter("postgres-password", true);
+var postgres = builder.AddPostgres("template-postgres", postgresUsername, postgresPassword)
+    .WithDataVolume(isReadOnly: false)
+    .WithLifetime(ContainerLifetime.Persistent);
+
 builder.AddProject<Template_Services_Bootstrapper>("template-bootstrapper");
 builder.AddProject<Template_Services_Gateway>("template-gateway");
 
